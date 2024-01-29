9effb4658ade5fbf13de9ba291eb0d5e14d7bef4
SherlonAlmeida <sherlonalmeidadasilva@gmail.com>
2024-01-29 14:12:25 -0500

    Dijkstra added

---
+++ GeneralFiles/visualizationGraphs-networkx-pt2.py
@@ -0,0 +1,13 @@
+#Reference: https://stackoverflow.com/questions/30035039/fix-position-of-subset-of-nodes-in-networkx-spring-graph
+
+import networkx as nx 
+import matplotlib.pyplot as plt 
+
+
+G = nx.Graph()
+G.add_edges_from([(1,2),(2,3),(3,1),(1,4)]) #define G
+fixed_positions = {1:(0,0),2:(-1,2)}#dict with two of the positions set
+fixed_nodes = fixed_positions.keys()
+pos = nx.spring_layout(G,pos=fixed_positions, fixed = fixed_nodes)
+nx.draw_networkx(G, pos)
+plt.show() 
---
+++ GeneralFiles/visualizationGraphs-networkx-pt3.py
@@ -0,0 +1,24 @@
+#Reference: https://www.geeksforgeeks.org/directed-graphs-multigraphs-and-visualization-in-networkx/
+
+import networkx as nx 
+import matplotlib.pyplot as plt 
+
+G = nx.DiGraph()
+G.add_edges_from([(1, 1), (1, 7), (2, 1), (2, 2), (2, 3),  
+                  (2, 6), (3, 5), (4, 3), (5, 4), (5, 8), 
+                  (5, 9), (6, 4), (7, 2), (7, 6), (8, 7)]) 
+  
+plt.figure(figsize =(6, 6)) 
+nx.draw_networkx(G, with_labels = True, node_color ='green') 
+  
+# getting different graph attributes 
+print("Total number of nodes: ", int(G.number_of_nodes())) 
+print("Total number of edges: ", int(G.number_of_edges())) 
+print("List of all nodes: ", list(G.nodes())) 
+print("List of all edges: ", list(G.edges())) 
+print("In-degree for all nodes: ", dict(G.in_degree())) 
+print("Out degree for all nodes: ", dict(G.out_degree)) 
+print("List of all nodes we can go to in a single step from node 2: ", list(G.successors(2)))   
+print("List of all nodes from which we can go to node 2 in a single step: ", list(G.predecessors(2))) 
+
+plt.show()
